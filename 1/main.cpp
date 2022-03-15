625dbd0f2cdcb30a4fe5cd4dff7634f6bfee9d41
pavukkkkk <kirilenkokonstantin2003@gmail.com>
2022-03-15 18:26:05 +0400

    1

---
+++ 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by Константин Кириленко on 15.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
